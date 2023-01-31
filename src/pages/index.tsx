702cea373258a7f4e51d4ba41641d204ec509353
Thayná Diniz <thaydinizf@gmail.com>
2023-01-31 20:06:27 -0300

    Estilização da tela de informações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,13 +18,13 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.home}>
           <Image
-            className={styles.backgroundImage} 
-            alt='' 
-            src={require('../../statics/we.jpg')} 
-            height={3750 / scaleDownBackground} 
+            className={styles.backgroundImage}
+            alt=''
+            src={require('../../statics/we.jpg')}
+            height={3750 / scaleDownBackground}
             width={3000 / scaleDownBackground}
           />
-          <div className={styles.homeText}> 
+          <div className={styles.homeText}>
             <h1 className={styles.title}>Guilherme & Thayná</h1>
             <nav className={styles.navigation}>
               <ul>
@@ -32,21 +32,23 @@ export default function Home() {
                   <a href="">Confirmar presença</a>
                 </li>
                 <li>
-                  <a href="">Informações</a>
+                  <a href="#information">Informações</a>
                 </li>
               </ul>
             </nav>
           </div>
         </div>
-        <div> 
-          <h2>Informações</h2> 
-          <div>
-            <h3>Cerimônia</h3>
-            <p>Será realizada às 17h do dia 14 de Outubro de 2023 na Igreja da Matriz</p>
-          </div>
-          <div>
-            <h3>Recepção</h3>
-            <p>A festa será realizada logo após a cerimônia, a partir de 19h no Mirante Clube</p>
+        <div className={styles.information}>
+          <h2 className={styles.title} id="information">Informações</h2>
+          <div className={styles.textInformation}>
+            <div>
+              <h3 className={styles.subtitle}>Cerimônia</h3>
+              <p>Será realizada às 17h do dia 14 de Outubro de 2023 na Igreja da Matriz</p>
+            </div>
+            <div>
+              <h3 className={styles.subtitle}>Recepção</h3>
+              <p>A festa será realizada logo após a cerimônia, a partir de 19h no Mirante Clube</p>
+            </div>
           </div>
         </div>
       </main>
