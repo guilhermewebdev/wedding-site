b610a5a24fd08754113ae5ce77e41d1c77ed4ea8
Guilherme Isaías <guilherme.silva@ferreri.co>
2023-04-24 21:44:31 -0300

    Ajuste na conexão com o banco

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,10 +5,11 @@ const {
     DB_NAME = '',
     DB_USER = '',
     DB_PASS = '',
+    DB_PROT = 'mongodb'
 } = process.env;
 
 export function createDBConnection() {
-    const dbURI = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}`
+    const dbURI = `${DB_PROT}://${DB_USER}:${DB_PASS}@${DB_HOST}`
     const client = new MongoClient(dbURI);
     return client.db(DB_NAME);
 }
