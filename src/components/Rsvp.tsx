106902938cee7302a64006ad8ad16e5cd09599eb
Guilherme Isaías <guilherme.silva@ferreri.co>
2023-07-05 21:54:59 -0300

    Feedback de confirmação

--- src/components/Rsvp.tsx
+++ src/components/Rsvp.tsx
@@ -1,19 +1,30 @@
 import Head from "next/head";
 import styles from '../../styles/Styles.module.css'
-import React from "react";
+import React, { useState } from "react";
 import { ConfirmAttendanceDTO, confirmAttendanceDTOimpl } from "../modules/attendance/DTOs/createGuest";
 import { buildUseForm } from "../hooks/useForm";
 import { apiClient } from "../lib/apiClient";
 import { Field } from "./Field";
 import Spinner from "./Spinner";
+import Modal from "./Modal";
 
+interface State {
+    isSuccess: boolean;
+}
+
+const defaultState: State = {
+    isSuccess: false,
+}
 
 const useForm = buildUseForm<ConfirmAttendanceDTO>(confirmAttendanceDTOimpl);
 
 export function RSVP() {
-    const { errors, register, onSubmit, loading } = useForm({ });
+    const { errors, register, onSubmit, loading } = useForm({});
+    const [state, setState] = useState<State>(defaultState)
+    const { isSuccess } = state;
     const submit = React.useCallback(async (form: ConfirmAttendanceDTO) => {
         await apiClient.post('/attendance', form);
+        setState({ ...state, isSuccess: true })
     }, []);
     return (
         <>
@@ -37,6 +48,14 @@ export function RSVP() {
                         }
                     </div>
                 </form>
+                <Modal show={isSuccess} onClose={() => setState({ ...state, isSuccess: false })}>
+                    <h3>Agradecemos a sua confirmação!</h3>
+                    <p>Será um prazer termos sua presença.</p>
+                    <p>Nos vemos na cerimônia</p>
+                    <p>
+                        <a className={styles.button} href="/">Ir para o início</a>
+                    </p>
+                </Modal>
             </main>
         </>
     )
